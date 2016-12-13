9531726885a7e4229666ee7e98385844649f9cc9
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2016-12-13 14:03:38 -0800

    Finished the stringifyJSON specs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,30 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var typeObj = typeof obj;
+  var str = '' +obj;
+  
+  if ( typeObj === 'number' || obj === null || typeObj === 'boolean') {
+  	return str;
+  }
+  if ( typeObj === 'string') {
+  	return '"' + obj + '"';
+  }
+  if ( Array.isArray (obj)) {
+  	var results = [];
+  	for ( var i = 0; i < obj.length; i++ ) {
+  		results.push( stringifyJSON( obj[i]));
+  	}
+  	return '[' + results + ']';
+  }
+  if ( typeObj === 'object') {
+  	var results = [];
+  	for ( var key in obj) {
+  	  if( key === 'functions' || key === undefined) {
+  	  	return '{}';
+  	  }	
+    results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]))
+  	}
+  	return '{' + results.join(',') + '}';
+  }
 };
